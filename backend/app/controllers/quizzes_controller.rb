6523c5b763e0ad76a286d74546001b5dbacd7bbe
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-09-21 16:32:29 -0400

    Adding updates

--- backend/app/controllers/quizzes_controller.rb
+++ backend/app/controllers/quizzes_controller.rb
@@ -1,13 +1,41 @@
 class QuizzesController < ApplicationController
   def index
+    @quiz = Quiz.all
+    render json: @quiz
   end
 
   def create
+    @quiz = Quiz.new(quiz_params)
+
+    if @quiz.save
+      render json: @quiz, status: :created, location: @quiz
+    else
+      render json: @quiz.errors, status: :unprocessable_entity
+    end
   end
 
-  def udpate
+  def edit 
+    @quiz = Quiz.find(params[:id])
+  end
+
+  def update
+    @quiz = Quiz.find(params[:id])
+    if @quiz.update
+      render json: @quiz, status: :created, location: @quiz
+    else
+      render json: @quiz.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @quiz = Quiz.find(params[:id])
+    @quiz.destroy
+    render json: @quiz
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:text)
   end
 end
