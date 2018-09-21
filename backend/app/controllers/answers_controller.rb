fda09140aee24e5ed7848e9fc1911d4baa4eeade
lstravers <stravers222@gmail.com>
2018-09-21 15:58:29 -0400

    add answers model

---
+++ backend/app/controllers/answers_controller.rb
@@ -0,0 +1,13 @@
+class AnswersController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def udpate
+  end
+
+  def destroy
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'answers/index'
+  get 'answers/create'
+  get 'answers/udpate'
+  get 'answers/destroy'
+  get 'quizzes/index'
+  get 'quizzes/create'
+  get 'quizzes/udpate'
+  get 'quizzes/destroy'
   get 'users/index'
   get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
