253082f65074fd1a0894f85e0732f4447c207fa5
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-09-22 13:17:38 -0400

    Updated user & app contollers so that in order to create a user API token must be entered in postman.

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
+  helper_method :current_user
+  helper_method :logged_in?
+  helper_method :api_token_user
+
+  # protect_from_forgery with: :null_session
   
   before_action :verify_authentication
 
@@ -14,5 +19,21 @@ class ApplicationController < ActionController::API
       @current_user = user
     end
   end
+
+
+  protected
+      def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      end
+  
+      def logged_in?
+        !!current_user
+      end
+
+      def api_token_user
+        @api_token_user ||= authenticate_with_http_token do |token, options|
+          User.find_by_api_token(token)
+        end
+      end
   
 end
--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  skip_before_action :verify_authentication
-  #before_action :set_user, only: [:show, :update, :destroy]
+  # skip_before_action :verify_authentication
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -9,11 +9,18 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    render json: @user 
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
-    @user = User.new(users_params)
+    @user = User.new(user_params)
 
     if @user.save
       render json: @user, status: :created, location: @user
@@ -32,17 +39,30 @@ class UsersController < ApplicationController
   end
 
   def destroy
-
+    # @user.destroy
+    # respond_to do |format|
+    #   format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+    #   format.json { render head :no_content }
+    # end
   end
 
-private
+  def verify_authentication
+    user = authenticate_with_http_token do |token, options|
+      User.find_by_api_token(token)
+    end
 
-  def set_user
-    @user = User.find(params[:id])
+    unless user
+      render json: { error: "You don't have permission to access these resources" }, status: :unauthorized
+    end
   end
 
-  def users_params
-    params.require(:user).permit(:username, :email, :password)
-  end
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 
 end
