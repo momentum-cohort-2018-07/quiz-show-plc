cf7a16158ab8af55cbcf2e2ea34ffc55282d3067
lstravers <stravers222@gmail.com>
2018-09-22 00:02:35 -0400

    modified users controller to show user route

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -5,8 +5,14 @@ class UsersController < ApplicationController
     render json: @users
   end
 
+  def show
+    @user = User.find(params[:id])
+    render json: @user 
+  end
+
+
   def create
-    @user = User.new(user_params)
+    @user = User.new(users_params)
 
     if @user.save
       render json: @user, status: :created, location: @user
