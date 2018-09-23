d1a582cebe1eb791199f6e05fba8c55c67564dd1
tcwhitak <t.chad.whitaker@gmail.com>
2018-09-23 14:19:30 -0400

    add superagent, remove create react app defaults

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Quiz Show PLC</h1>
+          <input className='signin_input' type='text' defaultValue='username' />
+          <input className='signin_input' type='text' defaultValue='password' />
+          <button>Sign In</button>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>By Monday:
+          <li>a React front-end that talks to a Rails API</li>
+          <li>API hosted on Heroku </li>
+          <li>front-end hosted on Firebase</li>
+          <li>Documentation of your intended API and database structure</li>
+          <li>React components you plan to use</li>
+          <li>ability to register users</li>
+          <li>ability to log in users</li>
+          <li>ability to see a list of quizzes</li>
+          <li>database with seed data</li>
+        </ul>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
