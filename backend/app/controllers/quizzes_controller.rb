7bed11d5c892b418fb68c57b47372e87bb46d6ca
lstravers <stravers222@gmail.com>
2018-09-23 13:26:51 -0400

    add show method to quiz controller

--- backend/app/controllers/quizzes_controller.rb
+++ backend/app/controllers/quizzes_controller.rb
@@ -2,49 +2,90 @@ class QuizzesController < ApplicationController
   skip_before_action :verify_authentication, only: [:index]
   before_action :set_quiz, only: [:show, :update, :destroy]
   
-  
   def index
-    @quiz = Quiz.all
-    render json: @quiz
+    @quizzes = Quiz.where('published = ?', true)
   end
 
-  def create
-    @quiz = Quiz.new(quiz_params)
+  def published_quizzes
+    @quizzes = Quiz.where({published: true, user_id: params[:user_id]})
+    render :index, location: api_quizzes_url
+  end
 
-    if @quiz.save
-      render json: @quiz, status: :created, location: @quiz
-    else
-      render json: @quiz.errors, status: :unprocessable_entity
-    end
+  def unpublished_quizzes
+    @quizzes = Quiz.where({published: false, user_id: params[:user_id]})
+    render :index, location: api_quizzes_url
   end
 
   def show
-    @quiz = Quiz.find(params[:id])
-    render json: @quiz
+    render json: @quizzes = Quiz.where(published: true)
   end
 
-  def edit 
-    @quiz = Quiz.find(params[:id])
+  def create
+    if !logged_in?
+      render json: {error: "Must be logged in to create a quiz"}, status: :unauthorized
+    elsif !current_user.admin
+      render json: {error: "Must be an admin to create a quiz"}, status: :unauthorized
+    else
+      @quiz = Quiz.new(title: quiz_params[:title], user_id: current_user.id)
+      if @quiz.save
+        render :show, status: :created, location: api_quiz_url(@quiz)
+      else
+        render json: @quiz.errors, status: :unprocessable_entity
+      end
+    end
   end
 
   def update
-    @quiz = Quiz.find(params[:id])
-    if @quiz.update
-      render json: @quiz, status: :created, location: @quiz
+    if current_user.id != @user.id
+      render json: {error: "Must be the owner to update this quiz"}, status: :unauthorized
     else
-      render json: @quiz.errors, status: :unprocessable_entity
+      if @quiz.update(quiz_params)
+        render :show, status: :updated, location: api_quiz_url(@quiz)
+      else
+        render json: @quiz.errors, status: :unprocessable_entity
+      end
+    end
+  end
+
+  def publish
+    @quiz = Quiz.find(params[:quiz_id])
+    @user = @quiz.user
+    if current_user.id != @user.id
+      render json: {error: "Must be the owner to publish this quiz"}, status: :unauthorized
+    elsif Answer.where(question_id: Question.where('quiz_id = ?', @quiz.id)).where('correct = ?', true).length < @quiz.questions.count
+      render json: {error: "All questions must have a correct answer before publishing"}, status: :unprocessable_entity
+    elsif Answer.where(question_id: Question.where('quiz_id = ?', @quiz.id)).where('correct = ?', true).length > @quiz.questions.count
+      render json: {error: "One of more questions have multiple correct answer. Please fix before publishing"}, status: :unprocessable_entity
+    else
+      if @quiz.update(published: true)
+        @quiz.questions.sort_by(&:created_at).each_with_index do |question, index|
+          question.update(number: index + 1)
+        end
+        render :show, status: :updated, location: api_quiz_url(@quiz)
+      else
+        render json: @quiz.errors, status: :unprocessable_entity
+      end
     end
   end
 
   def destroy
-    @quiz = Quiz.find(params[:id])
-    @quiz.destroy
-    render json: @quiz
+    if current_user.id != @user.id
+      render json: {error: "Must be the owner to delete this quiz"}, status: :unauthorized
+    elsif @quiz.published
+      render json: {error: "Cannot delete a published quiz"}, status: :unauthorized
+    else
+      @quiz.destroy
+    end
   end
 
   private
 
   def quiz_params
-    params.require(:quiz).permit(:title, :published)
+    params.permit(:title)
+  end
+
+  def set_quiz
+    @quiz = Quiz.find(params[:id])
+    #@user = @quiz.user
   end
 end
