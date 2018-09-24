f97e8e86bfd07307b0ebaebb3d29f8f6fbfd1252
tcwhitak <t.chad.whitaker@gmail.com>
2018-09-24 09:09:19 -0400

    added FE documentation and removed to do list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,56 +14,6 @@ class App extends Component {
           <RegisterForm />
         </header>
         <AllQuizzes />
-        <ul>Tasks Due By Monday:
-          <li>a React front-end that talks to a Rails API</li>
-          <ul>
-            <li>Status: Incomplete</li>
-            <li>Responsibility: Both</li>
-          </ul>
-          <li>API hosted on Heroku: <a href='https://blooming-savannah-88933.herokuapp.com/'>blooming-savannah-88933.herokuapp.com/</a> </li>
-          <ul>
-            <li>Status: Initial Deployment Complete, Deployments continue as we update</li>
-            <li>Responsibility: Back End</li>
-          </ul>
-          <li>front-end hosted on Firebase: <a href='https://quiz-show-plc.firebaseapp.com'>quiz-show-plc.firebaseapp.com</a> </li>
-          <ul>
-            <li>Status: Initial Deployment Complete, Deployments continue as we update</li>
-            <li>Responsibility: Front End</li>
-          </ul>
-          <li>Documentation of your intended API and database structure</li>
-          <ul>
-            <li>Status: Incomplete</li>
-            <li>Responsibility: Back End</li>
-          </ul>
-          <li>Documentation of React components you plan to use</li>
-          <ul>
-            <li>Status: Incomplete</li>
-            <li>Responsibility: Front End </li>
-          </ul>
-          <li>ability to register users</li>
-          <ul>
-            <li>Status: Incomplete</li>
-            <li>Responsibility: Both</li>
-            <li>FE NOTE: Registering currently logs Username and Password in the Console, use data.register(username, password) when API Calls are ready</li>
-          </ul>
-          <li>ability to log in users</li>
-          <ul>
-            <li>Status: Incomplete</li>
-            <li>Responsibility: Both</li>
-            <li>FE NOTE: LogIn currently logs Username and Password in the Console, use data.login(username, password) when API Calls are ready</li>
-          </ul>
-          <li>ability to see a list of quizzes</li>
-          <ul>
-            <li>Status: Incomplete</li>
-            <li>Responsibility: Both</li>
-            <li>FE NOTE: this now runs on a local dev rails server</li>
-          </ul>
-          <li>database with seed data</li>
-          <ul>
-            <li>Status: Incomplete</li>
-            <li>Responsibility: Back End</li>
-          </ul>
-        </ul>
       </div>
     )
   }
