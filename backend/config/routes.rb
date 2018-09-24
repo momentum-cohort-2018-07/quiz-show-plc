505ff82004b21d4b69232dd03485109abe30cf2c
lstravers <stravers222@gmail.com>
2018-09-23 22:36:01 -0400

    disregard this commit

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,18 +4,8 @@ Rails.application.routes.draw do
   
   root 'quizzes#index'
 
-
   resources :users
 
-<<<<<<< HEAD
-  # get 'answers/index'
-  # get 'answers/create'
-  # get 'answers/udpate'
-  # get 'answers/destroy'
-
-
-=======
->>>>>>> 7c142c414c6e12fc6b21b9595d43a892aba0e866
   resources :quizzes do 
     resources :questions do
       resources :answers
