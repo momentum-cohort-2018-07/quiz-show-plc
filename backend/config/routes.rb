d35ab2c53c686d637a7d5c999a64e2e4b32f0993
lstravers <stravers222@gmail.com>
2018-09-25 15:43:47 -0400

    nested quiz routes under api

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  get 'scores/index'
-  get 'scores/show'
-  get 'scores/create'
-  get 'scores/destroy'
+  # get 'scores/index'
+  # get 'scores/show'
+  # get 'scores/create'
+  # get 'scores/destroy'
+  
   namespace :api do
     resource :session, only: [:new, :create, :destroy]
   
     root 'quizzes#index'
     resources :users
-
+    resources :scores, only:[:index, :create, :show]
     resources :quizzes do 
       resources :questions do
         resources :answers
